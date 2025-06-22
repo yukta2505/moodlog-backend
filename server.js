9a0d9ae7dc5a48a0df284722b7b0218d0815ff1a
Yukta <baidyukta25@gmail.com>
2025-06-22 17:29:34 +0530

    Fix: Add CORS for frontend

--- server.js
+++ server.js
@@ -3,13 +3,15 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 
-const app = express({
+app.use(cors({
   origin: "https://moodlog-frontend.vercel.app",
   credentials: true
-});
+}));
+
+const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+
 app.use(express.json());
 
 const moodRoutes = require("./routes/moods");
